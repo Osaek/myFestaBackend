584d7ce389e6234c5be239fdb1d2cbfc27ea7ee6
sunwoo <kgw6147@naver.com>
2025-06-12 01:24:58 +0900

    :tada: Init: first commit

---
+++ src/main/java/com/oseak/myFestaBackend/FestabackendApplication.java
@@ -0,0 +1,13 @@
+package com.oseak.myFestaBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestabackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FestabackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oseak/myFestaBackend/FestabackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oseak.myFestaBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestabackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
