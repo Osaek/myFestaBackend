900ea4cc52f7e879f39cb0ea53d8882f6e2cafbe
sooyeon-kr <sooyeon7027@naver.com>
2025-06-19 20:10:38 +0900

    docs: 주석 수정

--- src/main/java/com/oseak/myFestaBackend/common/exception/code/BaseErrorCode.java
+++ src/main/java/com/oseak/myFestaBackend/common/exception/code/BaseErrorCode.java
@@ -5,7 +5,7 @@ import org.springframework.http.HttpStatus;
 /**
  * 모든 에러 코드 enum이 구현해야 하는 공통 인터페이스입니다.
  * - HTTP 상태 코드
- * - 커스텀 코드 (예: OSAEK-COM-001)
+ * - 커스텀 코드 (예: OSAEK-00001)
  * - 사용자 메시지
  */
 public interface BaseErrorCode {
@@ -16,7 +16,7 @@ public interface BaseErrorCode {
 	HttpStatus getHttpStatus();
 
 	/**
-	 * 비즈니스 용 커스텀 에러 코드 (예: "OSAEK-COM-001")
+	 * 비즈니스 용 커스텀 에러 코드 (예: OSAEK-00001)
 	 */
 	String getCode();
 
