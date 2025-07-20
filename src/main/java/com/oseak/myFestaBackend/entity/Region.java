c47122cac616dceffcaae99815a4ac89d8ed1a81
Elandland <68230815+Elandland@users.noreply.github.com>
2025-07-21 01:23:09 +0900

    fest : 지역 코드를 위한 Region entity, repository 추가

---
+++ src/main/java/com/oseak/myFestaBackend/entity/Region.java
@@ -0,0 +1,22 @@
+package com.oseak.myFestaBackend.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "region")
+@Getter
+@NoArgsConstructor
+public class Region {
+
+	@Id
+	@Column(name = "region_code")
+	private Integer regionCode;
+
+	@Column(name = "region_name", nullable = false)
+	private String regionName;
+}
