0f34bc19f2d89de2abde7916fb31e78003ce1268
mathos6147 <kgw6147@naver.com>
2025-07-23 20:27:40 +0900

    feat: Spring Security UserDetails 구현
    - CustomUserDetails : UserDetails 인터페이스 구현체
      SpringSecurity 인증에 필요한 권한, 계정 상태 정보 제공
    
    - MemberDetailsService : UserDetailsService 구현체
    
    - MemberPassword : 회원 비밀번호 엔티티

---
+++ src/main/java/com/oseak/myFestaBackend/domain/MemberPassword.java
@@ -0,0 +1,48 @@
+package com.oseak.myFestaBackend.domain;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@Table(name = "member_password")
+public class MemberPassword {
+
+	@Id
+	@Column(name = "member_id")
+	private Long memberId;
+
+	@Column(nullable = false)
+	private String password;
+
+	@CreationTimestamp
+	@Column(name = "created_at", updatable = false)
+	private LocalDateTime createdAt;
+
+	@UpdateTimestamp
+	@Column(name = "updated_at")
+	private LocalDateTime updatedAt;
+
+	@Builder
+	public MemberPassword(Long memberId, String password) {
+		this.memberId = memberId;
+		this.password = password;
+		this.createdAt = LocalDateTime.now();
+		this.updatedAt = LocalDateTime.now();
+	}
+
+	public void changePassword(String password) {
+		this.password = password;
+	}
+}
