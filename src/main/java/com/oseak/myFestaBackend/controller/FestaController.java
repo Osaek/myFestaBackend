035ae827ee88defe84576214f47473b8a1b415d0
Elandland <68230815+Elandland@users.noreply.github.com>
2025-08-02 23:23:55 +0900

    feat : 거리기반 축제 검색, 랜덤 축제 검색, conetentId를 통한 축제 검색 구현

--- src/main/java/com/oseak/myFestaBackend/controller/FestaController.java
+++ src/main/java/com/oseak/myFestaBackend/controller/FestaController.java
@@ -2,6 +2,7 @@ package com.oseak.myFestaBackend.controller;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.oseak.myFestaBackend.entity.dto.FestaSimpleDto;
+import com.oseak.myFestaBackend.entity.dto.FestaSummaryDto;
 import com.oseak.myFestaBackend.service.FestaService;
 
 import lombok.RequiredArgsConstructor;
@@ -20,14 +23,30 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/festival")
 public class FestaController {
 
-	//TODO : 추후 컨트롤러 삭제 후 배치파일로 메소드 작동할 예정
 	private final FestaService festaService;
 
+	//TODO : 배치로 전환 완료. 테스트용 API
 	@GetMapping("/fetch")
 	public ResponseEntity<String> fetchAndSaveFestivals(@RequestParam(required = false) Integer areaCode) {
 		String eventStartDate = LocalDate.now().format(DateTimeFormatter.BASIC_ISO_DATE);
 		festaService.fetchAndSaveFestivals(eventStartDate, areaCode);
 		return ResponseEntity.ok("축제 데이터 수집 및 저장 완료");
+	}
+
+	@GetMapping("/nearby")
+	public ResponseEntity<List<FestaSimpleDto>> getNearbyFestivalIds(@RequestParam double lat, @RequestParam double lng,
+		@RequestParam int distance) {
+		return ResponseEntity.ok(festaService.findNearbyFesta(lat, lng, distance));
+	}
+
+	@GetMapping("/summary")
+	public ResponseEntity<List<FestaSummaryDto>> getFestivalSummaries(@RequestParam List<Long> ids) {
+		return ResponseEntity.ok(festaService.getFestaSummariesByContentIds(ids));
+	}
+
+	@GetMapping("/random")
+	public ResponseEntity<List<FestaSimpleDto>> getRandomFestivals(@RequestParam int count) {
+		return ResponseEntity.ok(festaService.getRandomFestivals(count));
 	}
 
 }
