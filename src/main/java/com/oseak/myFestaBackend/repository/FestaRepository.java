2d216978f3629c760af81cf80a0f758f2e5c6e41
Elandland <68230815+Elandland@users.noreply.github.com>
2025-08-02 23:24:25 +0900

    feat : 거리기반 축제 검색(haversine사용), 랜덤 축제 검색(data 수 고려하여 random사용)

--- src/main/java/com/oseak/myFestaBackend/repository/FestaRepository.java
+++ src/main/java/com/oseak/myFestaBackend/repository/FestaRepository.java
@@ -1,9 +1,11 @@
 package com.oseak.myFestaBackend.repository;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.oseak.myFestaBackend.entity.Festa;
 
@@ -14,4 +16,22 @@ public interface FestaRepository extends JpaRepository<Festa, Long> {
 
 	Optional<Festa> findByContentId(Long contentId);
 
+	List<Festa> findAllByContentIdIn(List<Long> contentIds);
+
+	//Haversine공식
+	@Query(value = """
+		SELECT f.*
+		FROM festa f
+		WHERE (
+		  	6371 * acos(
+		    	cos(radians(:latitude)) * cos(radians(f.latitude)) *
+		    	cos(radians(f.longitude) - radians(:longitude)) +
+		    	sin(radians(:latitude)) * sin(radians(f.latitude))
+		  		)
+		) <= :distance
+		""", nativeQuery = true)
+	List<Festa> findByDistance(double latitude, double longitude, double distance);
+
+	@Query(value = "SELECT * FROM festa ORDER BY RAND() LIMIT :limit", nativeQuery = true)
+	List<Festa> findRandomFestivals(int limit);
 }
