643e78bc551257668ef8897436fc8f669833ee59
Elandland <68230815+Elandland@users.noreply.github.com>
2025-08-11 15:08:08 +0900

    feat. 개발자 추천 축제 entity추가

---
+++ src/main/java/com/oseak/myFestaBackend/repository/DevPickFestaRepository.java
@@ -0,0 +1,9 @@
+package com.oseak.myFestaBackend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.oseak.myFestaBackend.entity.DevPickFesta;
+
+public interface DevPickFestaRepository extends JpaRepository<DevPickFesta, Long> {
+
+}
