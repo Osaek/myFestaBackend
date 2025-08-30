5f6a2c303052008e66e5a5f3084928e0db930054
Elandland <68230815+Elandland@users.noreply.github.com>
2025-08-30 18:24:42 +0900

    edit. 랜덤조회, 거리기반 조회 시 종료된 축제는 처리되지 않도록 변경, 거리기반 조회시 거리 순으로 정렬되어 제공되도록 처리

--- src/main/java/com/oseak/myFestaBackend/repository/FestaRepository.java
+++ src/main/java/com/oseak/myFestaBackend/repository/FestaRepository.java
@@ -1,6 +1,5 @@
 package com.oseak.myFestaBackend.repository;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,7 +12,7 @@ import com.oseak.myFestaBackend.entity.Festa;
 public interface FestaRepository extends JpaRepository<Festa, Long>, JpaSpecificationExecutor<Festa> {
 	Optional<Festa> findByFestaName(String festaName);
 
-	Optional<Festa> findByFestaNameAndFestaStartAt(String festaName, LocalDateTime festaStartAt);
+	Optional<Festa> findByFestaNameAndFestaStartAt(String festaName, LocalDate festaStartAt);
 
 	List<Festa> findAllByFestaIdIn(List<Long> festaIds);
 
@@ -21,17 +20,32 @@ public interface FestaRepository extends JpaRepository<Festa, Long>, JpaSpecific
 	@Query(value = """
 		SELECT f.*
 		FROM festa f
-		WHERE (
-		  	6371 * acos(
-		    	cos(radians(:latitude)) * cos(radians(f.latitude)) *
-		    	cos(radians(f.longitude) - radians(:longitude)) +
-		    	sin(radians(:latitude)) * sin(radians(f.latitude))
-		  		)
-		) <= :distance
+		WHERE f.festa_status IN ('SCHEDULED','ONGOING')
+			AND f.latitude  IS NOT NULL
+		    AND f.longitude IS NOT NULL
+		    AND (
+		  		6371 * acos(
+		    		cos(radians(:latitude)) * cos(radians(f.latitude)) *
+		    		cos(radians(f.longitude) - radians(:longitude)) +
+		    		sin(radians(:latitude)) * sin(radians(f.latitude))
+		  			)
+			) <= :distance
+		ORDER BY
+		             6371 * acos(
+		               cos(radians(:latitude)) * cos(radians(f.latitude)) *
+		               cos(radians(f.longitude) - radians(:longitude)) +
+		               sin(radians(:latitude)) * sin(radians(f.latitude))
+		             ) ASC
 		""", nativeQuery = true)
 	List<Festa> findByDistance(double latitude, double longitude, double distance);
 
-	@Query(value = "SELECT * FROM festa ORDER BY RAND() LIMIT :limit", nativeQuery = true)
+	@Query(value = """
+		SELECT * 
+		FROM festa
+		WHERE festa_status IN ('SCHEDULED','ONGOING')
+		ORDER BY RAND()
+		LIMIT :limit
+		""", nativeQuery = true)
 	List<Festa> findRandomFestas(int limit);
 
 }
